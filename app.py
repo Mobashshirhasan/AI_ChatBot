799aa822ea929aaec1171b94105e8cd5e08faad7
Mobashshir Hasan <mobashshir12@gmail.com>
2024-11-10 10:21:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def index_get():
 @app.post("/predict")  #here we extract json
 def predict():
     text = request.get_json().get("message")
-      #TODO: check if text is valid
+    #TODO: check if text is valid
     response = get_response(text)
     message = {"answer": response}
     return jsonify(message)
